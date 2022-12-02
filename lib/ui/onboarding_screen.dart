02c3a36269c0d1e7b7856ea090fcab15685c76d2
Naim <naimkst@gmail.com>
2022-12-02 16:03:41 +0600

    Onboard Screen done

--- lib/ui/onboarding_screen.dart
+++ lib/ui/onboarding_screen.dart
@@ -1,5 +1,7 @@
 import 'package:firebasewithflutter/const/AppColors.dart';
 import 'package:flutter/material.dart';
+
+import 'login_screen.dart';
 class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({Key? key}) : super(key: key);
 
@@ -21,9 +23,7 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
         actions: [
           Padding(padding: const  EdgeInsets.only(right: 20, top: 20),
             child: InkWell(
-              onTap: ()=> {
-                print("Click!")
-              },
+              onTap: ()=> Navigator.push(context, MaterialPageRoute(builder: (_)=>const Login())),
               child: const Text('Skip', style: TextStyle(
                 color: Colors.grey,
                 fontSize: 16.0,
@@ -44,27 +44,125 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
             },
             controller: _pageController,
             children: [
-              Container(
-                padding: const EdgeInsets.only(left: 50, right: 50, bottom: 80),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(height: 350, child: Image.asset('assets/images/plant-one.png'),),
-                    const SizedBox(height: 20,),
-                    Text(Constants.titleOne, textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Constants.primaryColor,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-
-                    ),
-                    )
-                  ],
-                ),
-
+              createPage(
+                image: 'assets/images/plant-one.png',
+                title: Constants.titleOne,
+                description: Constants.descriptionOne,
+              ),
+              createPage(
+                image: 'assets/images/plant-two.png',
+                title: Constants.titleTwo,
+                description: Constants.descriptionTwo,
+              ),
+              createPage(
+                image: 'assets/images/plant-three.png',
+                title: Constants.titleThree,
+                description: Constants.descriptionThree,
               ),
             ],
           ),
+          Positioned(
+            bottom: 80,
+          left: 30,
+          child: Row(
+            children: _buildIndicator(),
+          ),
+          ),
+          Positioned(
+            bottom: 60,
+            right: 30,
+            child: Container(
+             child: IconButton(
+               onPressed: (){
+                 setState(() {
+                   if(currentIndex < 2){
+                     currentIndex ++;
+                     if(currentIndex < 3){
+                       _pageController.nextPage(duration: Duration(milliseconds: 300), curve: Curves.easeIn);
+                     }
+                   }else{
+                     Navigator.push(context, MaterialPageRoute(builder: (_)=>const Login()));
+                   }
+                 });
+               },
+               icon: const Icon(Icons.arrow_forward_ios,
+               size: 24,
+                 color: Colors.white,
+               ),
+             ),
+              padding: const EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Constants.primaryColor,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _indicator(bool isActive){
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: 10.0,
+      width: isActive ? 20: 8,
+      margin: const EdgeInsets.only(right: 5.0),
+      decoration: BoxDecoration(
+        color: Constants.primaryColor,
+        borderRadius: BorderRadius.circular(5),
+      ),
+    );
+  }
+
+  List<Widget> _buildIndicator(){
+    List<Widget> indicators = [];
+    for( int i=0; i < 3; i++){
+      if(currentIndex == i){
+        indicators.add(_indicator(true));
+      }else{
+        indicators.add(_indicator(false));
+      }
+    }
+    return indicators;
+  }
+}
+
+class createPage extends StatelessWidget {
+
+  final String image;
+  final String title;
+  final String description;
+
+  const createPage({
+    Key? key, required this.image, required this.title, required this.description
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 50, right: 50, bottom: 80),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(height: 350, child: Image.asset(image),),
+          const SizedBox(height: 20,),
+          Text(title, textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Constants.primaryColor,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+          ),
+          const SizedBox(height: 20,),
+          Text(description,
+          textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          const SizedBox(height: 20,),
         ],
       ),
     );
