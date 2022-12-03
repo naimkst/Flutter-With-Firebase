448a093607410bbe0ea0256d7d1e47af84cdc5bb
Naim <naimkst@gmail.com>
2022-12-04 03:50:46 +0600

    single details page done

--- lib/ui/screen/details_screen.dart
+++ lib/ui/screen/details_screen.dart
@@ -80,18 +80,29 @@ class _DetailsPageState extends State<DetailsPage> {
                     child: SizedBox(
                       height: 350,
                       child: Image.asset(_plantList[widget.plantId].imageURL),
-                  ),),
+                    ),
+                  ),
                   Positioned(
                     top: 10,
                     right: 0,
                     child: SizedBox(
+                      height: 150,
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children:  [
-                          PlantFuture(title: 'Size', plantFuture: _plantList[widget.plantId].size),
-                          PlantFuture(title: 'Humidity', plantFuture: _plantList[widget.plantId].humidity.toString()),
-                          PlantFuture(title: 'Temperature', plantFuture: _plantList[widget.plantId].temperature),
+                        children: [
+                          PlantFuture(
+                              title: 'Size',
+                              plantFuture: _plantList[widget.plantId].size),
+                          PlantFuture(
+                              title: 'Humidity',
+                              plantFuture: _plantList[widget.plantId]
+                                  .humidity
+                                  .toString()),
+                          PlantFuture(
+                              title: 'Temperature',
+                              plantFuture:
+                                  _plantList[widget.plantId].temperature),
                         ],
                       ),
                     ),
@@ -100,7 +111,139 @@ class _DetailsPageState extends State<DetailsPage> {
               ),
             ),
           ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              padding: const EdgeInsets.only(top: 80, left: 30, right: 30),
+              height: size.height * .5,
+              width: size.width,
+              decoration: BoxDecoration(
+                color: Constants.primaryColor.withOpacity(.4),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            _plantList[widget.plantId].plantName,
+                            style: TextStyle(
+                              color: Constants.primaryColor,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            r'$' + _plantList[widget.plantId].price.toString(),
+                            style: TextStyle(
+                                fontSize: 24,
+                                color: Constants.blackColor,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            _plantList[widget.plantId].rating.toString(),
+                            style: TextStyle(
+                              fontSize: 30,
+                              color: Constants.primaryColor,
+                            ),
+                          ),
+                          Icon(
+                            Icons.star,
+                            size: 30,
+                            color: Constants.primaryColor,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Expanded(
+                      child: Text(
+                    _plantList[widget.plantId].decription,
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                      height: 1.5,
+                      fontSize: 18,
+                      color: Constants.blackColor.withOpacity(.7),
+                    ),
+                  )),
+                ],
+              ),
+            ),
+          ),
         ],
+      ),
+      floatingActionButton: SizedBox(
+        width: size.width * .9,
+        height: 50,
+        child: Row(
+          children: [
+            Container(
+              height: 50,
+              width: 50,
+              child: Icon(
+                Icons.shopping_basket,
+                color: Colors.white,
+              ),
+              decoration: BoxDecoration(
+                color: Constants.primaryColor.withOpacity(.5),
+                borderRadius: BorderRadius.circular(50),
+                boxShadow: [
+                  BoxShadow(
+                    offset: const Offset(0, 1),
+                    blurRadius: 5,
+                    color: Constants.primaryColor.withOpacity(.3),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Constants.primaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 1),
+                          blurRadius: 5,
+                          color: Constants.primaryColor.withOpacity(.3))
+                    ]),
+                child: Center(
+                  child: Text(
+                    "Buy Now",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -109,9 +252,8 @@ class _DetailsPageState extends State<DetailsPage> {
 class PlantFuture extends StatelessWidget {
   final String plantFuture;
   final String title;
-  const PlantFuture({
-    Key? key, required this.plantFuture, required this.title
-  }) : super(key: key);
+  const PlantFuture({Key? key, required this.plantFuture, required this.title})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
