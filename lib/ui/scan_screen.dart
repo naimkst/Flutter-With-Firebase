49d6ca4c44c51fdc90590e4194c7a31c22a10106
Naim <naimkst@gmail.com>
2022-12-05 16:13:18 +0600

    Scan page done

--- lib/ui/scan_screen.dart
+++ lib/ui/scan_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebasewithflutter/const/AppColors.dart';
 import 'package:flutter/material.dart';
 
 class ScanPage extends StatefulWidget {
@@ -10,9 +11,85 @@ class ScanPage extends StatefulWidget {
 class _ScanPageState extends State<ScanPage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Scan Page"),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 50,
+            left: 20,
+            right: 20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Constants.primaryColor.withOpacity(.15),
+                    ),
+                    child: Icon(
+                      Icons.close,
+                      color: Constants.primaryColor,
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    print('widget.plantId');
+                  },
+                  child: Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Constants.primaryColor.withOpacity(.15),
+                    ),
+                    child: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          print('object');
+                        });
+                      },
+                      icon: Icon(Icons.share),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 100,
+            left: 20,
+            right: 20,
+            child: Container(
+              width: size.width * .8,
+              height: size.height * .8,
+              padding: EdgeInsets.all(20),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/images/code-scan.png', height: 100,),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text("Tap to Scan", style: TextStyle(
+                      color: Constants.primaryColor.withOpacity(.80),
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20
+                    ),),
+                  ],
+                ),
+              ),
+            ),),
+        ],
       ),
     );
   }
